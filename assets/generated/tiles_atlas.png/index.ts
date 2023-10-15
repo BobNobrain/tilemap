86d2acaeaefea8c173abf64da5edf323e75eaf0c
BobNobrain <bobnobrain@yandex.ru>
2023-10-15 22:44:59 +0300

    Finish first iteration of 'nebula' tile generation code

--- assets/generated/tiles_atlas.png/index.ts
+++ assets/generated/tiles_atlas.png/index.ts
@@ -2,12 +2,30 @@ import { createAtlas } from '../../generator/atlas';
 import { runImageGenerator } from '../../generator/image';
 import type { AssetGenerator } from '../../generator/types';
 import { ICY_BLUE_SHADES, ROCKY_BROWN_SHADES, ROCKY_GRAY_SHADES } from './colors';
+import { NebulaTilePalette, createNebulaTile } from './nebula';
 import { SolidTilePalette, createSolidTile } from './solid';
 
-function getTileCombinations(palette: SolidTilePalette) {
+function getSolidTileCombinations(palette: SolidTilePalette) {
     return [
         createSolidTile(palette, { leftSide: false, rightSide: false, leftBorder: false, rightBorder: false }),
         createSolidTile(palette, { leftSide: true, rightSide: true, leftBorder: true, rightBorder: true }),
+    ];
+}
+
+function getNebulaTileCombinations(palette: NebulaTilePalette) {
+    return [
+        createNebulaTile(palette, {
+            adjacentLeftBottom: true,
+            adjacentLeftTop: true,
+            adjacentRightBottom: true,
+            adjacentRightTop: true,
+        }),
+        createNebulaTile(palette, {
+            adjacentLeftBottom: false,
+            adjacentLeftTop: false,
+            adjacentRightBottom: false,
+            adjacentRightTop: false,
+        }),
     ];
 }
 
@@ -23,7 +41,14 @@ const GRAY_ROCK_PALETTE: SolidTilePalette = { primary: ROCKY_GRAY_SHADES };
 const BROWN_ROCK_PALETTE: SolidTilePalette = { primary: ROCKY_BROWN_SHADES, secondary: ROCKY_GRAY_SHADES };
 const ICE_PALETTE: SolidTilePalette = { primary: ICY_BLUE_SHADES };
 
+const COLD_NEBULA_PALETTE: NebulaTilePalette = { primary: ICY_BLUE_SHADES };
+
 export const generate: AssetGenerator = () => {
-    const atlas = createAtlas(transpose([GRAY_ROCK_PALETTE, BROWN_ROCK_PALETTE, ICE_PALETTE].map(getTileCombinations)));
+    const atlas = createAtlas(
+        transpose([
+            ...[GRAY_ROCK_PALETTE, BROWN_ROCK_PALETTE, ICE_PALETTE].map(getSolidTileCombinations),
+            ...[COLD_NEBULA_PALETTE].map(getNebulaTileCombinations),
+        ]),
+    );
     return runImageGenerator(atlas, { format: 'png' });
 };
--- assets/generated/tiles_atlas.png/nebula.ts
+++ assets/generated/tiles_atlas.png/nebula.ts
@@ -1,4 +1,5 @@
 import { RandomNumberGenerator } from '../../../src/lib/math/rng';
+import { remapLinear } from '../../../src/lib/math/linear';
 import { AtlasConstants } from '../../../src/renderer/tiles/atlas';
 import { ImageGenerator } from '../../generator/image';
 
@@ -35,29 +36,91 @@ const TILE_ZONES = ((): TileZone[][] => {
         [D, D, D, T, T, T, T, T, T, T, T, T, T, D, D, D],
         [D, T, T, T, T, T, T, T, T, T, T, T, T, T, T, D],
         [T, T, T, T, T, T, T, T, T, T, T, T, T, T, T, T],
-        [D, T, T, T, T, T, T, T, T, T, T, T, T, T, T, D],
-        [D, D, D, T, T, T, T, T, T, T, T, T, T, D, D, D],
-        [D, D, D, D, D, T, T, T, T, T, T, D, D, D, D, D],
-        [D, D, D, D, D, D, D, T, T, D, D, D, D, D, D, D],
-        [D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D],
-        [D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D],
-        [D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D],
-        [D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D],
-        [D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D],
-        [D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D],
-        [D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D],
-        [D, D, D, D, D, D, D, D, D, D, D, D, D, D, D, D],
+        [L, T, T, T, T, T, T, T, T, T, T, T, T, T, T, R],
+        [L, L, L, T, T, T, T, T, T, T, T, T, T, R, R, R],
+        [L, L, L, L, L, T, T, T, T, T, T, R, R, R, R, R],
+        [L, L, L, L, L, L, L, T, T, R, R, R, R, R, R, R],
+        [L, L, L, L, L, L, L, L, R, R, R, R, R, R, R, R],
+        [L, L, L, L, L, L, L, L, R, R, R, R, R, R, R, R],
+        [L, L, L, L, L, L, L, L, R, R, R, R, R, R, R, R],
+        [L, L, L, L, L, L, L, L, R, R, R, R, R, R, R, R],
+        [D, L, L, L, L, L, L, L, R, R, R, R, R, R, R, D],
+        [D, D, D, L, L, L, L, L, R, R, R, R, R, D, D, D],
+        [D, D, D, D, D, L, L, L, R, R, R, D, D, D, D, D],
+        [D, D, D, D, D, D, D, L, R, D, D, D, D, D, D, D],
     ];
 })();
 
+const MASS_CENTER_X = AtlasConstants.TILE_WIDTH / 2 - 0.5;
+const MASS_CENTER_Y = (AtlasConstants.TOP_SIDE_HEIGHT * 2) / 3 - 0.5;
+const EMPTY_PIXELS_SHARE = 0.2;
+
 export function createNebulaTile(palette: NebulaTilePalette, opts: NebulaTileOptions): ImageGenerator {
-    const rng = new RandomNumberGenerator('nebula').detached();
+    const rng = new RandomNumberGenerator('pretty-nebula');
+    const nColors = palette.primary.length;
+    const maxNoise = Math.floor(nColors / 3);
 
     return {
         getSize: () => ({
             width: AtlasConstants.TILE_WIDTH,
-            height: AtlasConstants.TILE_FULL_HEIGHT,
+            height:
+                opts.adjacentLeftBottom && opts.adjacentRightBottom
+                    ? AtlasConstants.TOP_SIDE_HEIGHT
+                    : AtlasConstants.TILE_FULL_HEIGHT,
         }),
-        draw: (canvas) => {},
+        draw: (canvas) => {
+            const ctx = canvas.getContext('2d');
+
+            for (let x = 0; x < AtlasConstants.TILE_WIDTH; ++x) {
+                for (let y = 0; y < AtlasConstants.TILE_FULL_HEIGHT; ++y) {
+                    const zone = TILE_ZONES[y][x];
+
+                    if (
+                        zone === TileZone.Dead ||
+                        (zone === TileZone.LeftSide && opts.adjacentLeftBottom) ||
+                        (zone === TileZone.RightSide && opts.adjacentRightBottom)
+                    ) {
+                        continue;
+                    }
+
+                    const rand = rng.forInts([x, y]);
+                    if (rand() < EMPTY_PIXELS_SHARE) {
+                        continue;
+                    }
+
+                    const xZone = x < MASS_CENTER_X ? 0 : 1;
+                    const yZone = y < MASS_CENTER_Y ? 0 : 2;
+
+                    const hasAdjacent = [
+                        opts.adjacentLeftTop,
+                        opts.adjacentRightTop,
+                        opts.adjacentLeftBottom,
+                        opts.adjacentRightBottom,
+                    ][xZone + yZone];
+
+                    let fadeFactor = 0;
+                    if (!hasAdjacent) {
+                        const dx = MASS_CENTER_X - x;
+                        const dy = MASS_CENTER_Y - y; // should fade out ~twice as fast, due to view angle
+                        fadeFactor = Math.sqrt(dx * dx + dy * dy) / AtlasConstants.TILE_SIDE_WIDTH;
+                    }
+
+                    const colorIndex = remapLinear(fadeFactor, { start: 1.3, end: 0 }, { start: 0, end: nColors });
+                    const noise = remapLinear(rand(), { start: 0, end: 1 }, { start: -maxNoise, end: maxNoise });
+
+                    const noisedColorIndex = Math.floor(Math.max(0, Math.min(colorIndex + noise, nColors)));
+
+                    if (noisedColorIndex < 0) {
+                        continue;
+                    } else if (noisedColorIndex < nColors) {
+                        ctx.fillStyle = palette.primary[noisedColorIndex];
+                    } else {
+                        ctx.fillStyle = palette.primary[nColors - 1];
+                    }
+
+                    ctx.fillRect(x, y, 1, 1);
+                }
+            }
+        },
     };
 }
